177ce6e7034cc2602805ce53c338b475351df39c
Erzhuyijian <1821344102@qq.com>
2020-04-28 22:46:38 +0800

    navTheme: 'light

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -2,10 +2,10 @@
  * @Author: Wenzhe
  * @Date: 2020-04-08 16:25:28
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-09 07:54:47
+ * @LastEditTime: 2020-04-14 13:40:23
  */
 export default {
-  navTheme: 'dark',
+  navTheme: 'light',
   // 拂晓蓝
   primaryColor: '#1890ff',
   layout: 'sidemenu',
