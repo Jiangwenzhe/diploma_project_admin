b549163db36f13138eabcff9b0b1739359c955c2
Erzhuyijian <1821344102@qq.com>
2020-05-12 21:41:44 +0800

    admin 系统信息界面添加系统日志系统

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,32 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Card, Typography, Alert, Row, Col, Statistic } from 'antd';
+import { Card, Row, Col, Statistic, Button } from 'antd';
+import { SyncOutlined } from '@ant-design/icons';
+import request from '@/utils/request';
+import { useRequest } from '@umijs/hooks';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import styles from './Welcome.less';
 
-export default () => (
-  <PageHeaderWrapper>
-    <Row gutter={12}>
-      <Col span={6}>
-        <Card title="所用用户">
-        <Statistic title="用户数量" value={12} suffix="/ 人" />
-        </Card>
-      </Col>
-      <Col span={6}>
-        <Card title="最近比赛">
-          <Statistic title="比赛数量" value={8} suffix="/ 个" />
-        </Card>
-      </Col>
-      <Col span={6}>
-        <Card title="今日提交">
-          <Statistic title="submission" value={112893} />
-        </Card>
+export default () => {
+  const [logInfo, setLogInfo] = useState('fetching logs ... ... ');
+  const [isFetchLog, setIsFetchLog] = useState(false);
 
+  useEffect(() => {
+    request('/api/log').then((response) => {
+      setLogInfo(response.data);
+    });
+  }, []);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { data, loading, run, cancel } = useRequest('/api/log', {
+    manual: true,
+    pollingInterval: 1000,
+    pollingWhenHidden: false,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setLogInfo(data.data);
+    }
+  }, [data]);
+
+  const startRun = () => {
+    run();
+    setIsFetchLog(true);
+  };
+
+  const cancelRun = () => {
+    cancel();
+    setIsFetchLog(false);
+  };
+
+  return (
+    <PageHeaderWrapper>
+      <Row gutter={12}>
+        <Col span={6}>
+          <Card title="所用用户">
+            <Statistic title="用户数量" value={12} suffix="/ 人" />
+          </Card>
         </Col>
-      <Col span={6}>
-        <Card title="今日访问">
-          <Statistic title="访问量" value={112893} />
-        </Card>
-      </Col>
-    </Row>
-  </PageHeaderWrapper>
-);
+        <Col span={6}>
+          <Card title="最近比赛">
+            <Statistic title="比赛数量" value={8} suffix="/ 个" />
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="今日提交">
+            <Statistic title="submission" value={112893} />
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="今日访问">
+            <Statistic title="访问量" value={112893} />
+          </Card>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: '25px' }}>
+        <Col span={24}>
+          <Card
+            title="系统日志"
+            extra={
+              <div>
+                <Button onClick={startRun}>开始轮询获取 log</Button>
+                <Button onClick={cancelRun} style={{ marginLeft: '5px' }}>
+                  结束
+                </Button>
+                <Button shape="circle" type="dashed" style={{ marginLeft: '5px' }}>
+                  <SyncOutlined spin={isFetchLog} />
+                </Button>
+              </div>
+            }
+          >
+            <div className={styles.logBlock}>
+              <SyntaxHighlighter style={dark}>{logInfo}</SyntaxHighlighter>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </PageHeaderWrapper>
+  );
+};
